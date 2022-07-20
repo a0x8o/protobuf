17a6eaaf7c0d884658fb3f5cdc9d4a7c375ef5f1
Alex <alex.barreto@databricks.com>
2022-07-20 11:46:58 -0500

    Merge pull request #6201 from haon4/3.8.x-201905311515
    Merge 3.8.x into master branch

--- src/google/protobuf/compiler/java/java_enum.cc
+++ src/google/protobuf/compiler/java/java_enum.cc
@@ -44,6 +44,7 @@
 #include <google/protobuf/io/printer.h>
 #include <google/protobuf/stubs/strutil.h>
 
+
 namespace google {
 namespace protobuf {
 namespace compiler {
@@ -77,10 +78,9 @@ void EnumGenerator::Generate(io::Printer* printer) {
   WriteEnumDocComment(printer, descriptor_);
   MaybePrintGeneratedAnnotation(context_, printer, descriptor_, immutable_api_);
   printer->Print(
-      "$deprecation$public enum $classname$\n"
+      "public enum $classname$\n"
       "    implements com.google.protobuf.ProtocolMessageEnum {\n",
-      "classname", descriptor_->name(), "deprecation",
-      descriptor_->options().deprecated() ? "@java.lang.Deprecated " : "");
+      "classname", descriptor_->name());
   printer->Annotate("classname", descriptor_);
   printer->Indent();
 
@@ -143,13 +143,9 @@ void EnumGenerator::Generate(io::Printer* printer) {
     vars["number"] = StrCat(descriptor_->value(i)->number());
     vars["{"] = "";
     vars["}"] = "";
-    vars["deprecation"] = descriptor_->value(i)->options().deprecated()
-                              ? "@java.lang.Deprecated "
-                              : "";
     WriteEnumValueDocComment(printer, descriptor_->value(i));
     printer->Print(vars,
-                   "$deprecation$public static final int ${$$name$_VALUE$}$ = "
-                   "$number$;\n");
+                   "public static final int ${$$name$_VALUE$}$ = $number$;\n");
     printer->Annotate("{", "}", descriptor_->value(i));
   }
   printer->Print("\n");
@@ -179,9 +175,6 @@ void EnumGenerator::Generate(io::Printer* printer) {
       "}\n"
       "\n"
       "/**\n"
-      " * @param value The numeric wire value of the corresponding enum "
-      "entry.\n"
-      " * @return The enum associated with the given numeric wire value.\n"
       " * @deprecated Use {@link #forNumber(int)} instead.\n"
       " */\n"
       "@java.lang.Deprecated\n"
@@ -189,11 +182,6 @@ void EnumGenerator::Generate(io::Printer* printer) {
       "  return forNumber(value);\n"
       "}\n"
       "\n"
-      "/**\n"
-      " * @param value The numeric wire value of the corresponding enum "
-      "entry.\n"
-      " * @return The enum associated with the given numeric wire value.\n"
-      " */\n"
       "public static $classname$ forNumber(int value) {\n"
       "  switch (value) {\n",
       "classname", descriptor_->name());
@@ -233,25 +221,7 @@ void EnumGenerator::Generate(io::Printer* printer) {
   if (HasDescriptorMethods(descriptor_, context_->EnforceLite())) {
     printer->Print(
         "public final com.google.protobuf.Descriptors.EnumValueDescriptor\n"
-        "    getValueDescriptor() {\n");
-    if (SupportUnknownEnumValue(descriptor_->file())) {
-      if (ordinal_is_index) {
-        printer->Print(
-            "  if (this == UNRECOGNIZED) {\n"
-            "    throw new java.lang.IllegalStateException(\n"
-            "        \"Can't get the descriptor of an unrecognized enum "
-            "value.\");\n"
-            "  }\n");
-      } else {
-        printer->Print(
-            "  if (index == -1) {\n"
-            "    throw new java.lang.IllegalStateException(\n"
-            "        \"Can't get the descriptor of an unrecognized enum "
-            "value.\");\n"
-            "  }\n");
-      }
-    }
-    printer->Print(
+        "    getValueDescriptor() {\n"
         "  return getDescriptor().getValues().get($index_text$);\n"
         "}\n"
         "public final com.google.protobuf.Descriptors.EnumDescriptor\n"
@@ -302,22 +272,15 @@ void EnumGenerator::Generate(io::Printer* printer) {
       // for every enum.
       printer->Print("values();\n");
     } else {
-      printer->Print("getStaticValuesArray();\n");
-      printer->Print("private static $classname$[] getStaticValuesArray() {\n",
-                     "classname", descriptor_->name());
-      printer->Indent();
       printer->Print(
-          "return new $classname$[] {\n"
-          "  ",
-          "classname", descriptor_->name());
+          "{\n"
+          "  ");
       for (int i = 0; i < descriptor_->value_count(); i++) {
         printer->Print("$name$, ", "name", descriptor_->value(i)->name());
       }
       printer->Print(
           "\n"
           "};\n");
-      printer->Outdent();
-      printer->Print("}");
     }
 
     printer->Print(
