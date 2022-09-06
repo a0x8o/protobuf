0c0fbb43c6c26922bc6ef5d1c97b3e70e86f85b9
Alex <alex.barreto@databricks.com>
2022-09-06 13:15:08 -0500

    Merge pull request #6201 from haon4/3.8.x-201905311515
    Merge 3.8.x into master branch

--- python/google/protobuf/pyext/map_container.h
+++ python/google/protobuf/pyext/map_container.h
@@ -33,7 +33,6 @@
 
 #include <Python.h>
 
-#include <cstdint>
 #include <memory>
 
 #include <google/protobuf/descriptor.h>
@@ -55,9 +54,12 @@ struct MapContainer : public ContainerBase {
   // Use to get a mutable message when necessary.
   Message* GetMutableMessage();
 
+  // Cache some descriptors, used to convert keys and values.
+  const FieldDescriptor* key_field_descriptor;
+  const FieldDescriptor* value_field_descriptor;
   // We bump this whenever we perform a mutation, to invalidate existing
   // iterators.
-  uint64_t version;
+  uint64 version;
 };
 
 struct MessageMapContainer : public MapContainer {
